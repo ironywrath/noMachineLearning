aab188b9816bbc0bd98e33e6dcbb51b2581a0884
geniuschinchillas <geniuschinchillas@gmail.com>
2018-12-10 09:15:08 -0700

    AAPL

--- CH7_StockData.py
+++ CH7_StockData.py
@@ -9,6 +9,7 @@ def read_stock_data(stockcode, start_date, stop_date):
 def get_stockcol_series(stockdata, col_name):
     return stockdata[col_name]
 
-def get_stats(s)
+def get_stats(s):
+    
 
 #print("hello world")
