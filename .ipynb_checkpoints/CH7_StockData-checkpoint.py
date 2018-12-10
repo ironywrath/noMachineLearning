3cc2a14eb8178dc93780f68fcb3ef2923e526425
ChinchillasTaraToro <30887754+geniuschinchillas@users.noreply.github.com>
2018-12-09 19:50:24 -0700

    appl

---
+++ .ipynb_checkpoints/CH7_StockData-checkpoint.py
@@ -0,0 +1,9 @@
+import  numpy
+import pandas
+from pandas_datareader import data, wb
+import pandas_datareader as pdr
+
+def ReadStockData(stockcode, start_date, stop_date):
+    return 0
+
+print("hello world")
--- CH7_StockData.py
+++ CH7_StockData.py
@@ -1,9 +1,14 @@
-import  numpy
+import numpy
 import pandas
 from pandas_datareader import data, wb
 import pandas_datareader as pdr
 
-def ReadStockData(stockcode, start_date, stop_date):
-    return 0
+def read_stock_data(stockcode, start_date, stop_date):
+    return pdr.data.get_data_yahoo(stockcode, start_date, stop_date)
 
-print("hello world")
+def get_stockcol_series(stockdata, col_name):
+    return stockdata[col_name]
+
+def get_stats(s)
+
+#print("hello world")
